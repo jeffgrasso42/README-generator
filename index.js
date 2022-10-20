839b8757ed2d62b32b2d99c973624b53845f53cf
JAG <jagrasso42@gmail.com>
2022-10-20 18:10:53 -0400

    added questions to questions array, added inquirer prompts for the questions in the init function

--- index.js
+++ index.js
@@ -4,14 +4,71 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js')
 
+// DATA
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  "Enter the title of your project: ",
+  "Enter a description of your project: ",
+  "Enter Installation instructions for your project: ",
+  "Enter Usage information for your project: ",
+  "Enter Contribution guidelines for your project: ",
+  "Enter Testing instructions for your project: ",
+  "Select Liscence: "
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: questions[0],
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: questions[1],
+        name: 'description',
+      },
+      {
+        type: 'input',
+        message: questions[2],
+        name: 'install',
+      },
+      {
+        type: 'input',
+        message: questions[3],
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: questions[4],
+        name: 'contribute',
+      },
+      {
+        type: 'input',
+        message: questions[5],
+        name: 'test',
+      },
+      {
+        type: 'list',
+        message: questions[6],
+        choices: 
+          [
+            'none',
+            'Academic Free License v3.0',
+            'Apache license 2.0',
+            'MIT',
+            'PostgreSQL License'
+          ]
+      }
+    ]).then(response => {
+      data = generateMarkdown(data);
+    })
+}
 
 // Function call to initialize app
 init();
