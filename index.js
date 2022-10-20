b5948254bff0abad36f40c93cbf8d6e670bd72fc
JAG <jagrasso42@gmail.com>
2022-10-20 17:30:00 -0400

    initialized npm, installed necessary packages, added package dependencies to index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // TODO: Include packages needed for this application
+// DEPENDENCIES
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
 const questions = [];
