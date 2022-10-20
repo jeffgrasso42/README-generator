3da62ef179c5073cbb03c4f7a8f7359d1b57c61b
JAG <jagrasso42@gmail.com>
2022-10-20 18:24:48 -0400

    added error handling to fs.writFile

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data);
+  fs.writeFile(fileName, data, (err) => {
+    err ? console.error(err) : console.log('Success!');
+  });
 }
 
 // TODO: Create a function to initialize app
